fd09216c8ed11092392d28bded7e33902f3a9d8d
Greg <greg9381@gmail.com>
2018-03-10 16:00:59 -0500

    added initial version of engine and outlines of agents

---
+++ agents.py
@@ -0,0 +1,18 @@
+class Speaker:
+    def __init__(self, ideology_variance, diplomacy_variance, x, y):
+        self.x = x
+        self.y = y
+        # TODO: use variances to initialize ideology and diplomacy levels
+
+    def interacts_with(self, passerby):
+        pass
+        # TODO
+
+class Passerby:
+    def __init__(self, ideology_variance, diplomacy_variance):
+        pass
+        # TODO: use variances to initialize ideology and diplomacy levels
+
+    def interacts_with(self, passerby):
+        pass
+        # TODO
