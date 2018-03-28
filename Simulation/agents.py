0eebfa64e0c8b5b14113e7091bd599c51ffb2d55
Thor Keller <tkeller34@gatech.edu>
2018-03-28 18:13:53 -0400

    Revert "removing Student instance variables x and y"
    This reverts commit 0be72767f76d988397b37f5a8d31f26a2fb46eac.

--- Simulation/agents.py
+++ Simulation/agents.py
@@ -26,6 +26,8 @@ class Speaker:
 
 class Student:
     def __init__(self, ideology, diplomacy, uid):
+        self.x = None
+        self.y = None
         self.ideology = max(-100, min(100, ideology))
         self.diplomacy = max(0, min(100, diplomacy))
         self.uid = "Student_" + str(uid + 1)
@@ -34,8 +36,12 @@ class Student:
         self.has_interacted = true
         # TODO
 
+    def set_position(self, x, y):
+        self.x = x
+        self.y = y
+
     def __str__(self):
-        return "Student with ideology {}".format(self.ideology)
+        return "({},{},{})".format(self.x, self.y, self.ideology)
 
     def __repr__(self):
-        return "Student with ideology {}".format(self.ideology)
+        return "Student: ({},{},{})".format(self.x, self.y, self.ideology)
