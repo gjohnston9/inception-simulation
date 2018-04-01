d3f5e45fde0924fcdbb6e4a2338e58f6be37e723
Greg <greg9381@gmail.com>
2018-03-31 23:02:45 -0400

    changing std devs from 25 to 10, adding lots of experiments

--- Simulation/run_simulation.py
+++ Simulation/run_simulation.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python2
 from model import Model
 
-experiment_name = "test1"
-
-population_density = 0.3
 students_to_speakers_ratio = 20
 speaker_range = 2
 
@@ -12,29 +9,80 @@ expected_speaker_diplomacy = 75
 expected_abs_student_ideology = 30
 expected_abs_speaker_ideology = 80
 
-student_diplomacy_std_deviation = 25
-speaker_diplomacy_std_deviation = 25
-student_ideology_std_deviation = 25
-speaker_ideology_std_deviation = 25
+student_diplomacy_std_deviation = 10
+speaker_diplomacy_std_deviation = 10
+student_ideology_std_deviation = 10
+speaker_ideology_std_deviation = 10
 
 random_seed = 10
 
-test_model = Model(
-    experiment_name,
+experiment_name = "pop_density_test"
+for population_density in (0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9):
+    test_model = Model(
+        experiment_name,
 
-    population_density,
-    students_to_speakers_ratio,
-    speaker_range,
+        population_density,
+        students_to_speakers_ratio,
+        speaker_range,
 
-    expected_student_diplomacy,
-    expected_speaker_diplomacy,
-    expected_abs_student_ideology,
-    expected_abs_speaker_ideology,
+        expected_student_diplomacy,
+        expected_speaker_diplomacy,
+        expected_abs_student_ideology,
+        expected_abs_speaker_ideology,
 
-    student_diplomacy_std_deviation,
-    speaker_diplomacy_std_deviation,
-    student_ideology_std_deviation,
-    speaker_ideology_std_deviation,
+        student_diplomacy_std_deviation,
+        speaker_diplomacy_std_deviation,
+        student_ideology_std_deviation,
+        speaker_ideology_std_deviation,
 
-    random_seed)
-test_model.run_model()
+        random_seed)
+    test_model.run_model()
+
+
+experiment_name = "speaker_ideology_test"
+population_density = 0.3
+for expected_abs_speaker_ideology in (30, 40, 50, 60, 70, 80, 90, 95):
+    test_model = Model(
+        experiment_name,
+
+        population_density,
+        students_to_speakers_ratio,
+        speaker_range,
+
+        expected_student_diplomacy,
+        expected_speaker_diplomacy,
+        expected_abs_student_ideology,
+        expected_abs_speaker_ideology,
+
+        student_diplomacy_std_deviation,
+        speaker_diplomacy_std_deviation,
+        student_ideology_std_deviation,
+        speaker_ideology_std_deviation,
+
+        random_seed)
+    test_model.run_model()
+
+
+experiment_name = "student_ideology_test"
+population_density = 0.3
+expected_abs_speaker_ideology = 80
+for expected_abs_student_ideology in (10, 15, 20, 25, 30, 40, 50, 60, 70, 80, 90, 95):
+    test_model = Model(
+        experiment_name,
+
+        population_density,
+        students_to_speakers_ratio,
+        speaker_range,
+
+        expected_student_diplomacy,
+        expected_speaker_diplomacy,
+        expected_abs_student_ideology,
+        expected_abs_speaker_ideology,
+
+        student_diplomacy_std_deviation,
+        speaker_diplomacy_std_deviation,
+        student_ideology_std_deviation,
+        speaker_ideology_std_deviation,
+
+        random_seed)
+    test_model.run_model()
